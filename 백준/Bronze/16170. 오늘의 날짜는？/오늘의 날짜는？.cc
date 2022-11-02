90007bc4adceb03d05efc7fa7c4b7341ae3facf4
sinbla78 <103175928+sinbla78@users.noreply.github.com>
2022-11-03 08:17:30 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.cc
@@ -1,11 +1,7 @@
 #include <stdio.h>
-#include <time.h>
-int main()
-{
-    time_t timer;
-    struct tm *t;
-    timer = time(NULL);
-    t = gmtime(&timer);
-    printf("%d\n%d\n%d", t->tm_year + 1900, t->tm_mon+1, t->tm_mday);
- 
+
+int main(void) {
+	printf("%04d\n", 2022);
+	printf("%02d\n", 11);
+	printf("%02d\n", 2);
 }
