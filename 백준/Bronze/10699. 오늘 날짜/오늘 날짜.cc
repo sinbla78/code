1f0ff918d77d97ed01c51a60667b70beaa714c17
sinbla78 <103175928+sinbla78@users.noreply.github.com>
2022-11-03 08:23:45 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -1,12 +1,5 @@
-#include<stdio.h>
-#include<time.h>
+#include <stdio.h>
 
-int main() {
-    struct tm *t;
-    time_t timer; 
-    timer = time(NULL);
-    t = localtime(&timer);
-
-    printf("%d-%02d-%02d",t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
-    return 0;
+int main(void){
+    printf("2022-11-03");
 }
