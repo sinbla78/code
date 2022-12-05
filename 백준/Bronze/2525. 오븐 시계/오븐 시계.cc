d6697d264fd3e4f3ce6944304780af708a054b4b
sinbla78 <103175928+sinbla78@users.noreply.github.com>
2022-12-06 08:17:35 +0900

    [Bronze III] Title: 오븐 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) {
+	int h, m, t;
+	scanf("%d %d %d", &h, &m, &t);
+    h += t / 60;
+    m += t % 60;
+    if (m >= 60) {
+        h += 1;
+        m -= 60;
+    }
+    if (h >= 24) {
+        h -= 24;
+    }
+    printf("%d %d", h, m);
+    return 0;
+}
